e01db51a5327608c75412b73d0ea0e3cca0b0eb8
aziz <bentayeb78@hotmail.fr>
2023-11-10 14:30:54 +0100

    first commit

---
+++ BLOG/blog.php
@@ -0,0 +1,40 @@
+<?php 
+
+    include 'inc/header.inc.php';
+    include 'inc/nav.inc.php';
+
+?>
+
+
+    <main>
+        <div class="container border my-3 p-3">
+            <h1 class="p-3">Boutique</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro facere neque sunt. Dolor itaque inventore delectus eos, doloribus magni? Maiores doloribus aspernatur adipisci et molestias quam at nobis similique amet.
+                Distinctio unde mollitia libero consectetur quis exercitationem doloribus temporibus assumenda maiores sunt ut aliquid debitis placeat delectus quisquam molestias corporis possimus, obcaecati a architecto voluptatibus, est amet eaque. Et, soluta?
+                Ducimus dolorem quam necessitatibus. Culpa, doloribus quae laboriosam cupiditate voluptatem impedit praesentium voluptatibus quibusdam perspiciatis obcaecati nostrum dolores quod consequuntur et libero dolorem asperiores consequatur earum perferendis recusandae! Nobis, facere?
+                Quo atque molestiae, voluptatem quod, ullam mollitia molestias nam laudantium dolorem perferendis distinctio reprehenderit pariatur adipisci quis aut ipsam praesentium quae repellendus? Nobis illo, autem eligendi eveniet culpa libero labore.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro facere neque sunt. Dolor itaque inventore delectus eos, doloribus magni? Maiores doloribus aspernatur adipisci et molestias quam at nobis similique amet.
+                Distinctio unde mollitia libero consectetur quis exercitationem doloribus temporibus assumenda maiores sunt ut aliquid debitis placeat delectus quisquam molestias corporis possimus, obcaecati a architecto voluptatibus, est amet eaque. Et, soluta?
+                Ducimus dolorem quam necessitatibus. Culpa, doloribus quae laboriosam cupiditate voluptatem impedit praesentium voluptatibus quibusdam perspiciatis obcaecati nostrum dolores quod consequuntur et libero dolorem asperiores consequatur earum perferendis recusandae! Nobis, facere?
+                Quo atque molestiae, voluptatem quod, ullam mollitia molestias nam laudantium dolorem perferendis distinctio reprehenderit pariatur adipisci quis aut ipsam praesentium quae repellendus? Nobis illo, autem eligendi eveniet culpa libero labore.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro facere neque sunt. Dolor itaque inventore delectus eos, doloribus magni? Maiores doloribus aspernatur adipisci et molestias quam at nobis similique amet.
+                Distinctio unde mollitia libero consectetur quis exercitationem doloribus temporibus assumenda maiores sunt ut aliquid debitis placeat delectus quisquam molestias corporis possimus, obcaecati a architecto voluptatibus, est amet eaque. Et, soluta?
+                Ducimus dolorem quam necessitatibus. Culpa, doloribus quae laboriosam cupiditate voluptatem impedit praesentium voluptatibus quibusdam perspiciatis obcaecati nostrum dolores quod consequuntur et libero dolorem asperiores consequatur earum perferendis recusandae! Nobis, facere?
+                Quo atque molestiae, voluptatem quod, ullam mollitia molestias nam laudantium dolorem perferendis distinctio reprehenderit pariatur adipisci quis aut ipsam praesentium quae repellendus? Nobis illo, autem eligendi eveniet culpa libero labore.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro facere neque sunt. Dolor itaque inventore delectus eos, doloribus magni? Maiores doloribus aspernatur adipisci et molestias quam at nobis similique amet.
+                Distinctio unde mollitia libero consectetur quis exercitationem doloribus temporibus assumenda maiores sunt ut aliquid debitis placeat delectus quisquam molestias corporis possimus, obcaecati a architecto voluptatibus, est amet eaque. Et, soluta?
+                Ducimus dolorem quam necessitatibus. Culpa, doloribus quae laboriosam cupiditate voluptatem impedit praesentium voluptatibus quibusdam perspiciatis obcaecati nostrum dolores quod consequuntur et libero dolorem asperiores consequatur earum perferendis recusandae! Nobis, facere?
+                Quo atque molestiae, voluptatem quod, ullam mollitia molestias nam laudantium dolorem perferendis distinctio reprehenderit pariatur adipisci quis aut ipsam praesentium quae repellendus? Nobis illo, autem eligendi eveniet culpa libero labore.
+            </p>
+        </div>
+    </main>
+
+<?php 
+    include 'inc/footer.inc.php';
---
+++ BLOG/inc/footer.inc.php
@@ -0,0 +1,4 @@
+<footer class="bg-dark text-white pt-3 text-center pb-5">Copyright &copy; <?php echo date('d/m/Y') ?></footer>
+</body>
+
+</html>
